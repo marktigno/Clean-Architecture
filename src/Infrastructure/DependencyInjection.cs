bba23acc3f06f6e30b9a8a8df633607c6de0c645
Mark Joseph Tigno <markj_tigno@hotmail.com>
2026-05-19 17:01:39 +0800

    Enabled PostgreSQL.

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Domain.Repositories;
+using Infrastructure.Database;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Data;
@@ -11,11 +13,21 @@ namespace Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            // Use this if running on Docker MSSQL
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("DockerMsSqlConnectionString")));
+
             // Use this if running on MSSQLLocalDB
-            // services.AddDbContext<ApplicationDbContext>(options =>
-            //     options.UseSqlServer(configuration.GetConnectionString("LocalDbConnectionString")));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("LocalDbConnectionString")));
+
+            // Use this if running on Docker Postgres
+            //services.AddDbContext<ApplicationDbContext>(
+            //options => options
+            //    .UseNpgsql(configuration.GetConnectionString("DockerPostgresConnectionString"), npgsqlOptions =>
+            //        npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Default))
+            //    .UseSnakeCaseNamingConvention());
+
             services.AddScoped<IUnitOfWork>(factory => factory.GetRequiredService<ApplicationDbContext>());
             services.AddScoped<IDbConnection>(factory => factory.GetRequiredService<ApplicationDbContext>().Database.GetDbConnection());
             services.AddScoped<IRepository, TodoEntryRepository>();
